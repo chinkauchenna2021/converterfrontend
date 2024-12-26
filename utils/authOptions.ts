28f4a201dcfe51e7a68fa88c940fc22659888276
chinkauchenna2021 <uchennachinka4@gmail.com>
2024-12-26 22:04:08 +0100

    fix next api

---
+++ utils/authOptions.ts
@@ -0,0 +1,41 @@
+import type { AuthOptions, NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_DRIVE_CLIENT_ID!,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_DRIVE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code"
+                }
+            }
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+      },
+
+      secret: process.env.NEXT_PUBLIC_AUTH_SECRET as string,
+      callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.provider = account.provider;
+                token.accessToken = account.access_token;
+              }
+              return token;
+        },
+        async session({ session, token }) {
+            if (token.provider) {
+                session.user.provider = token.provider as string; 
+              }
+              if (token.accessToken) {
+                session.user.accessToken = token.accessToken as string;
+              }
+              return session;
+            },
+          }
+} as AuthOptions
